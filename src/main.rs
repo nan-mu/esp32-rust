fc6dda634d161d2673cb3828ca1c5bed2987990f
nan mu <mu.nan.11@outlook.com>
2023-09-05 09:29:17 +0800

    开始写bug

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::io::{Result, Write};
+use std::net::{self, Ipv4Addr, TcpStream};
+
+fn tcp_client(&str: req, Ipv4Addr: addr) -> Result<()> {
+    let client = TcpStream::connect(addr).expect("无法连接目标服务器");
+    client.write_all(req);
+}
+
+fn main() {
+    tcp_client(_, Ipv4Addr);
+}
